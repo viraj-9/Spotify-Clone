f8e6d086d3c2d0be50c792a4df3d620d1d52647a
virajOR <rokadeviraja09@gmail.com>
2026-09-22 16:50:47 +0530

    Added music player functionality

---
+++ script.js
@@ -0,0 +1,112 @@
+console.log("till 31st august");
+
+async function getSongs() {
+     let a = await fetch("assets/songs/")
+     let response = await a.text();
+     console.log(response)
+
+     let div = document.createElement('div')
+     div.innerHTML = response;
+     let as = div.getElementsByTagName('a')
+     console.log(as)
+     let songs = []
+     for (let index = 0; index < as.length; index++) {
+          const element = as[index];
+          if (element.href.endsWith('mp3')) {
+               let filename = decodeURIComponent(
+                    new URL(element.href).pathname.split("/").pop()
+               );
+               console.log(filename)
+               songs.push({
+                    name: filename,
+                    url: element.href
+               });
+          }
+
+     }
+     return songs;
+}
+
+async function main() {
+     let songs = await getSongs();
+     let currentSong = new Audio();
+     let playbar = document.querySelector(".playbar");
+     let lastbox1 = document.querySelector(".lastbox1");
+     let songImage = document.querySelector("#song");
+     let songTitle = document.querySelector(".btitle1");
+     let songArtist = document.querySelector(".btitle2");
+     console.log(songs);
+
+     let playbutton = document.querySelector(".play");
+     let downplaybutton = playbar.querySelector(".pause");
+     let pc = document.querySelector(".play-pc");
+     let cards = document.querySelectorAll(".robx");
+
+     cards.forEach(function (card, index) {
+          card.addEventListener("click", function () {
+               if (currentSong.paused) {
+                    pc.src = "/assets/images/continue.svg";
+                    
+
+               }
+               let poster = card.querySelector(".poster");
+               let title = card.querySelector(".title1").innerText;
+               let artist = card.querySelector(".title2").innerText;
+
+
+
+               let song = songs.find(function (song) {
+                    let cleanFileName = song.name
+                         .replace(".mp3", "")
+                         .toLowerCase()
+                         .replace(/[^a-z0-9]/g, "");
+
+                    let cleanTitle = title
+                         .toLowerCase()
+                         .replace(/[^a-z0-9]/g, "");
+
+                    console.log("Title: ", cleanTitle);
+                    console.log("File name: ", cleanFileName);
+                    return cleanTitle == cleanFileName;
+               })
+
+               if (!song) {
+
+                    console.log("Song not found:", title);
+
+                    return;
+
+               }
+               currentSong.src = song.url;
+               currentSong.play();
+               playbar.style.display = "flex";
+               lastbox1.style.display = "none";
+
+
+
+
+
+               songImage.src = poster.src;
+               songTitle.innerText = title;
+               songArtist.innerText = artist;
+          })
+
+     });
+
+     downplaybutton.addEventListener("click", function () {
+          if (currentSong.paused) {
+               currentSong.play();
+               pc.src = "/assets/images/continue.svg";
+
+          } else {
+               currentSong.pause();
+
+               pc.src = "/assets/images/pause.svg";
+               
+
+          }
+
+     });
+
+}
+main()
